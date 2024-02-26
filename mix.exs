2db8a60f9e9a39edd505b9ba0d352c72b5ceac49
Josh Чернов <961877+joshchernoff@users.noreply.github.com>
2024-02-26 08:58:16 -0800

    feat: Add github workflow

--- mix.exs
+++ mix.exs
@@ -21,8 +21,11 @@ defmodule Joshchernoff.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:nimble_publisher, "~> 1.1.0"},
+      {:phoenix_live_view, "~> 0.20.9"},
+      {:esbuild, "~> 0.8.1"},
+      {:tailwind, "~> 0.2.2"},
+      {:yaml_elixir, "~> 2.9.0"}
     ]
   end
 end
