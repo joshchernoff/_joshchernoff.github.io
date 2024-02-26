c5a45e280b3e41f1c84f37cd9af4863ce6cc13c2
Josh Чернов <961877+joshchernoff@users.noreply.github.com>
2024-02-26 08:43:05 -0800

    Init repo

---
+++ lib/joshchernoff.ex
@@ -0,0 +1,18 @@
+defmodule Joshchernoff do
+  @moduledoc """
+  Documentation for `Joshchernoff`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Joshchernoff.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/joshchernoff_test.exs
@@ -0,0 +1,8 @@
+defmodule JoshchernoffTest do
+  use ExUnit.Case
+  doctest Joshchernoff
+
+  test "greets the world" do
+    assert Joshchernoff.hello() == :world
+  end
+end
